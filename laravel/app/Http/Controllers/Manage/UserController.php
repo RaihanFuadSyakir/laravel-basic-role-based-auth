085c6a10bfa3c057731274d5673889e1caafe56c
RaihanFuadSyakir <142875762+RaihanFuadSyakir@users.noreply.github.com>
2025-08-26 06:21:23 +0700

    fix update users controller validation

--- laravel/app/Http/Controllers/Manage/UserController.php
+++ laravel/app/Http/Controllers/Manage/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rule;
 use Inertia\Inertia;
 class UserController extends Controller
@@ -49,13 +50,12 @@ class UserController extends Controller
             'name' => 'required|string|min:2|max:50',
             'email' => 'required|email',
             'change_password' => 'required|boolean',
-            'password' => [ Rule::requiredIf($request->boolean('change_password')),'string','min:8','max:100',],
+            'password' => 'exclude_unless:change_password,true|string|min:8|max:100',
         ]);
         $user = User::findOrFail($validated['id']);
         if ($validated['change_password']) {
-            $user->password = Hash::make($validated['password']);
+            $validated['password'] = Hash::make($validated['password']);
         }
-
         $user->update($validated);
         return redirect()->route('manage_users')
              ->with('success', 'User updated successfully');
