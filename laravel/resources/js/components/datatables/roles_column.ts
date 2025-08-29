01cdddaa4f24ab4d190dc104969c2330d56f6cee
RaihanFuadSyakir <142875762+RaihanFuadSyakir@users.noreply.github.com>
2025-08-29 21:05:12 +0700

    add sort in manage role

--- laravel/resources/js/components/datatables/roles_column.ts
+++ laravel/resources/js/components/datatables/roles_column.ts
@@ -2,25 +2,72 @@
 import { h } from 'vue';
 import type { ColumnDef } from '@tanstack/vue-table';
 import { Role} from '@/types';
+import { Button } from '@/components/ui/button';
+import { ArrowUpDown, Pencil, Trash2 } from 'lucide-vue-next';
 
-export const roleColumns: ColumnDef<Role>[] = [
+export const roleColumns = ({hasEditPermission, onEdit, onDelete}:
+    {
+        hasEditPermission : Boolean;
+        onEdit : (role : Role) => void;
+        onDelete : (role : Role) => void;
+    }) : ColumnDef<Role>[] => [
   {
     accessorKey: 'name',
-    header: () => h('div', { class: 'font-medium' }, 'Name'),
+     header: ({ column }) => {
+            return h('div', { class: 'flex justify-center items-center' }, [
+              h('span', 'Name'),
+              h(Button, {
+                variant: 'ghost',
+                class: 'ml-2',
+                onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+              }, () => h(ArrowUpDown, { class: 'h-4 w-4' }))
+            ])
+        },
     cell: ({ row }) => h('div', {}, row.getValue('name')),
   },
   {
-    accessorKey: 'permissions',
-    header: () => h('div', { class: 'font-medium' }, 'Permissions'),
+    accessorKey : 'permissions',
+    header: ({column}) => h('div', { class: 'font-medium' }, [
+        h('span','Total Permissions'),
+        h(Button, {
+            variant: 'ghost',
+            class: 'ml-2',
+            onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+        }, () => h(ArrowUpDown, { class: 'h-4 w-4' }))
+    ]),
     cell: ({ row }) => {
     const permissions = row.getValue('permissions') as string[] | null | undefined;
-
-    // If permissions is nullish or an empty array, show fallback.
-    if (!permissions || permissions.length === 0) {
-      return h('div', { class: 'text-sm text-muted-foreground' }, '—');
-    }
-
-    return h('div', {}, permissions.join(', '));
+    return h('div', {}, permissions?.length);
     },
   },
+  ...(hasEditPermission ? [{
+    id: "actions", // no accessorKey since it's not from data
+    header: () => h("div", { class: "font-medium" }, "Actions"),
+    cell: ({ row }) => {
+      const role = row.original as Role
+
+      return h("div", { class: "gap-2" }, [
+        h(
+          Button,
+          {
+            variant: "outline",
+            size: "sm",
+            class : "bg-blue-500 hover:bg-blue-400",
+            onClick: () => onEdit(role),
+          },
+            { default: () => h(Pencil, { class: "text-white w-4 h-4" }) }
+        ),
+        h(
+          Button,
+          {
+            variant: "destructive",
+            size: "sm",
+            class : "bg-red-500 hover:bg-red-400",
+            onClick: () => onDelete(role),
+          },
+            { default: () => h(Trash2, { class: "w-4 h-4" }) }
+        ),
+      ])
+    },
+  }] : []),
 ];
--- laravel/resources/js/pages/roles/ManageRole.vue
+++ laravel/resources/js/pages/roles/ManageRole.vue
@@ -1,25 +1,100 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
-import { Role, type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import { AppPageProps, Role, type BreadcrumbItem } from '@/types';
+import { Head, router, usePage } from '@inertiajs/vue3';
 import { roleColumns } from '@/components/datatables/roles_column';
 import DataTable from '@/components/datatables/DataTable.vue';
+import { computed } from 'vue';
+import { ref } from 'vue';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination/index"
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Manage Roles',
         href: '/manage_roles',
     },
 ];
-    const props = defineProps({roles : Array});
-    const roles = props.roles as Role[];
-    console.log(roles[0]);
+const page = usePage< AppPageProps & {
+ data : {
+  roles : Role[]
+ },
+  pagination: {
+    total: number,
+    currentPage: number,
+    lastPage: number
+  },
+}>();
+
+const pagination = computed(() => page.props.pagination);
+const roles = computed(() => page.props.data.roles);
+const hasEditPermission = page.props.auth.permissions.some(
+  (perm: string) => perm === "edit_roles"
+)
+function onEdit(role : Role){
+}
+function onDelete(role : Role){
+}
+const filters = ref<Record<string, string | string[]>>({});
+function fetchData(){
+  router.get('/manage_roles', { page : pagination.value.currentPage, ...filters.value }, { preserveState: true, preserveScroll: true })
+}
+const goToPage = (page: number) => {
+  if (page < 1 || page > pagination.value.lastPage) return
+  fetchData();
+}
+const column = roleColumns({
+    hasEditPermission : hasEditPermission,
+    onEdit : onEdit,
+    onDelete : onDelete
+    });
 </script>
 <template>
     <Head title="Manage Roles" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-            <DataTable :columns="roleColumns" :data="roles" />
+        <div class="p-4">
+            <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
+                <DataTable :columns="column" :data="roles" />
+            </div>
+            <div v-if="pagination" class="flex justify-end">
+                <Pagination
+                  v-slot="{ page }"
+                  :items-per-page="10"
+                  :total="pagination.total"
+                  :default-page="pagination.currentPage"
+                >
+                  <PaginationContent v-slot="{ items }">
+                    <PaginationPrevious
+                      :disabled="pagination.currentPage === 1"
+                      @click="goToPage(pagination.currentPage - 1)"
+                    />
+
+                    <template v-for="(item, index) in items" :key="index">
+                      <PaginationItem
+                        v-if="item.type === 'page'"
+                        :value="item.value"
+                        :is-active="item.value === pagination.currentPage"
+                        @click="goToPage(item.value)"
+                      >
+                        {{ item.value }}
+                      </PaginationItem>
+                    </template>
+
+                    <PaginationEllipsis v-if="pagination.lastPage > 5" />
+
+                    <PaginationNext
+                      :disabled="pagination.currentPage === pagination.lastPage"
+                      @click="goToPage(pagination.currentPage + 1)"
+                    />
+                  </PaginationContent>
+                </Pagination>
+            </div>
         </div>
     </AppLayout>
 </template>
