7c55693e0055897204ac00d8815cbf47e3ccc753
RaihanFuadSyakir <142875762+RaihanFuadSyakir@users.noreply.github.com>
2025-08-31 21:54:56 +0700

    add filter by permission at manage role

--- laravel/app/Http/Controllers/RoleController.php
+++ laravel/app/Http/Controllers/RoleController.php
@@ -2,25 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permission;
 use App\Models\Role;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 class RoleController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $roles = Role::with('permissions:name')
-            ->get();
-        $result = [];
-        foreach($roles as $role){
-            $result[] = [
+        $nameFilter = $request->query('name');
+        $permissionsFilter = $request->query('permission');
+        $perPage = $request->input('per_page', 10);
+        $roles = Role::with('permissions:name');
+        if($nameFilter){
+            $roles = $roles->where('name','like',"%{$nameFilter}%");
+        }
+        if ($permissionsFilter) {
+            // Split the comma-separated string into an array
+            $permissionsArray = explode(',', $permissionsFilter);
+
+            // Filter users by roles
+            $roles = $roles->whereHas('permissions', function ($query) use ($permissionsArray) {
+                $query->whereIn('name', $permissionsArray);
+            });
+        }
+        $roles = $roles->paginate($perPage)
+            ->appends($request->all());
+        $roles->getCollection()->transform(function ($role) {
+            return [
                 'name' => $role->name,
                 'permissions' => $role->permissions()->pluck('name')->toArray(),
             ];
-        };
-        return Inertia::render('roles/ManageRole',
-        [
-            'roles' => $result
-        ]);
+        });
+        $permissions = Permission::all()->pluck('name')->toArray();
+        $toSend = [
+            'data' => [
+                'roles' => $roles->items(),
+                'permissions' => $permissions
+            ],
+            'pagination' => [
+                'total' => $roles->total(),
+                'currentPage' => $roles->currentPage(),
+                'lastPage' => $roles->lastPage()
+            ]
+        ];
+        return Inertia::render('roles/ManageRole',$toSend);
+    }
+    public function  create (Request $request){
+    }
+    public function  update (Request $request){
+    }
+    public function  delete (Request $request){
     }
 }
+
--- laravel/resources/js/pages/roles/ManageRole.vue
+++ laravel/resources/js/pages/roles/ManageRole.vue
@@ -14,15 +14,23 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination/index"
+import Input from '@/components/ui/input/Input.vue';
+import { Filter } from 'lucide-vue-next';
+import { useForm } from 'vee-validate';
+import { formUpdateSchema } from '@/lib/roles/roles_schema';
+import { toTypedSchema } from '@vee-validate/zod';
+import { toast } from 'vue-sonner';
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Manage Roles',
         href: '/manage_roles',
     },
 ];
+import TagsCombobox from '@/components/TagsCombobox.vue';
 const page = usePage< AppPageProps & {
  data : {
-  roles : Role[]
+  roles : Role[],
+  permissions : string[]
  },
   pagination: {
     total: number,
@@ -36,14 +44,75 @@ const roles = computed(() => page.props.data.roles);
 const hasEditPermission = page.props.auth.permissions.some(
   (perm: string) => perm === "edit_roles"
 )
-function onEdit(role : Role){
+const formUpdate = useForm({
+    validationSchema : toTypedSchema(formUpdateSchema),
+    initialValues : {
+        id : 0,
+        name : '',
+        permissions : []
+    }
+});
+const roleTarget = ref<Role | null>(null);
+const isEditOpen = ref(false);
+const confirmDelete = ref(false);
+function onEdit(role: Role) {
+    roleTarget.value = role;
+    formUpdate.setValues({
+        id : roleTarget.value.id,
+        name : roleTarget.value.name,
+        permissions : roleTarget.value.permissions,
+    });
+    isEditOpen.value = true;
 }
-function onDelete(role : Role){
+
+function onDelete(role : Role) {
+    roleTarget.value = role;
+    confirmDelete.value = true;
+  // e.g. router.delete(`/users/${id}`)
 }
-const filters = ref<Record<string, string | string[]>>({});
+const handleUpdateSubmit = formUpdate.handleSubmit((values) => {
+    router.put(`/roles/${values.id}`,values,{
+        onSuccess : () => {
+            toast.success('Role updated successfully');
+            fetchData();
+            isEditOpen.value = false;
+            roleTarget.value = null;
+            formUpdate.resetForm();
+        },
+        onError : () => {
+            toast.error('Update unexpected error');
+        }
+    })
+  })
+const handleDeleteSubmit = () =>{
+    if(roleTarget.value){
+        router.delete(`/roles/${roleTarget.value.id}`,{
+            onSuccess : () => {
+                toast.success('Role deleted successfully');
+                fetchData();
+                confirmDelete.value = false;
+                roleTarget.value = null;
+            },
+            onError : () => {
+                toast.error('Update unexpected error');
+            }
+        });
+    }
+}
 function fetchData(){
   router.get('/manage_roles', { page : pagination.value.currentPage, ...filters.value }, { preserveState: true, preserveScroll: true })
 }
+function addFilter(key: string, value: string | string[]) {
+  if (Array.isArray(value)) {
+    filters.value = { ...filters.value, [key]: value.join(',') };
+  } else {
+    filters.value = { ...filters.value, [key]: value };
+  }
+  fetchData();
+}
+const filters = ref<Record<string, string | string[]>>({});
+const permissionsFilter = ref<string[]>([]);
+const permissions = page.props.data.permissions;
 const goToPage = (page: number) => {
   if (page < 1 || page > pagination.value.lastPage) return
   fetchData();
@@ -59,6 +128,25 @@ const column = roleColumns({
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="p-4">
+            <div class="grid md:grid-cols-3 gap-4">
+                <div class="md:ml-4 m-2 flex items-center">
+                    <div class="relative flex items-center flex-1">
+                      <!-- Icon inside input -->
+                      <Filter class="absolute left-2 size-4 opacity-50 pointer-events-none" />
+
+                      <!-- Input with padding so text doesn't overlap icon -->
+                      <Input
+                        type="text"
+                        placeholder="Filter by name"
+                        class="pl-8"
+                        @input="(e: any) => addFilter('name', e.target.value)"
+                      />
+                    </div>
+                </div>
+                <div class="flex justify-center m-2">
+                    <TagsCombobox class="flex-1" v-model="permissionsFilter" :options="permissions" placeholder="Filter By Permissions"/>
+                </div>
+            </div>
             <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
                 <DataTable :columns="column" :data="roles" />
             </div>
