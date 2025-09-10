62323392e5d5635c95581887b043c05e736f10d9
RaihanFuadSyakir <142875762+RaihanFuadSyakir@users.noreply.github.com>
2025-09-10 19:40:31 +0700

    disable landing page

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Landing');
+    return route.redirect('login');
 })->name('home');
 Route::middleware(['auth','verified'])->group(function(){
     Route::get('dashboard', function () {
